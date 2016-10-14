36e3bcacf16a55c982a30c21c8e83855acfb5ca5
Pavel Zhukov <cray0000@gmail.com>
2016-10-14 06:28:05 +0200

    Make react a peer dep. Simplify component to not render anything

--- EmptyComponent.js
+++ EmptyComponent.js
@@ -1,22 +1,7 @@
-import React, { Component } from 'react';
+const React = require('react')
 
-export default class EmptyComponent extends Component {
-
-  constructor (props) {
-    super(props);
-  }
-
-  componentDidMount () {
-    let nodes = document.getElementsByTagName('template');
-    for ( let i = 0 ; i < nodes.length ; i++ ) {
-      nodes[i].parentNode.removeChild(nodes[i]);
-    }
-  }
-
+export default class EmptyComponent extends React.Component {
   render () {
-    return (
-      <template />
-    );
+    return false
   }
-
 }
